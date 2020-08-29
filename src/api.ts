16d905a241d5a0134fe74f89649f80cc78c0b627
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-08-28 17:21:23 -0700

    feat: add options for changing direction of compile

--- src/api.ts
+++ src/api.ts
@@ -4,19 +4,47 @@ import { Parser } from "./Parser";
 import { Tokenizer } from "./Tokenizer";
 
 interface HacklangResult {
-  ast: Record<string, any>;
   code: string | null;
 }
 
-export function compile(input: string): HacklangResult {
-  const tokey = new Tokenizer(input, new ErrorHandler(input), Language.JAVASCRIPT);
-  tokey.tokenize();
+interface CompileOptions {
+  outputLang?: "hacklang" | "javascript";
+}
 
-  const parsey = new Parser(tokey.bag, Language.JAVASCRIPT);
-  parsey.parse();
+export function compile(input: string, options: CompileOptions = {}): HacklangResult {
+  const toHacklang = (input: string): Parser => {
+    const tokey = new Tokenizer(input, new ErrorHandler(input), Language.HACKLANG);
+    tokey.tokenize();
+
+    const parsey = new Parser(tokey.bag, Language.HACKLANG);
+    parsey.parse();
+
+    return parsey;
+  };
+  const toJavaScript = (input: string): Parser => {
+    const tokey = new Tokenizer(input, new ErrorHandler(input), Language.JAVASCRIPT);
+    tokey.tokenize();
+
+    const parsey = new Parser(tokey.bag, Language.JAVASCRIPT);
+    parsey.parse();
+
+    return parsey;
+  };
+
+  let parser: Parser | Record<string, any> = {};
+  if (options.outputLang === void 0) {
+    parser = toJavaScript(input);
+  } else if (options.outputLang === "hacklang") {
+    parser = toHacklang(input);
+  } else if (options.outputLang === "javascript") {
+    parser = toJavaScript(input);
+  }
+
+  if (parser && parser.output === undefined) {
+    parser.output = null;
+  }
 
   return {
-    ast: tokey.bag,
-    code: parsey.output,
+    code: parser.output,
   };
 }
